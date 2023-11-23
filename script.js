a92823931f1fe195bad4cea40eda7c9dc29d8da8
Carlos Alberto <carlos.neurozze@gmail.com>
2023-11-23 10:39:17 -0300

    start project

---
+++ script.js
@@ -0,0 +1,30 @@
+const mario = document.querySelector('.mario');
+const cano = document.querySelector('.cano');
+
+const pulo = () => {
+    mario.classList.add('pulo');
+    setTimeout(() => {
+        mario.classList.remove('pulo');
+    }, 500)
+}
+
+const loop = setInterval(() => {
+    const canoPosition = cano.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+    if (canoPosition <= 120 && canoPosition > 0 && marioPosition < 80) {
+        cano.style.animation = 'none';
+        cano.style.left = `${canoPosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = './imagens/game-over.png';
+        mario.style.width = '75px';
+        mario.style.marginLeft = '50px';
+
+        clearInterval(loop);
+    }
+}, 10);
+
+document.addEventListener('keydown', pulo);
