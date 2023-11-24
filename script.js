fcb005d8e1ba538e8760bbb6b1c7068404966c09
Carlos Alberto <carlos.neurozze@gmail.com>
2023-11-23 21:39:01 -0300

    adjustments

--- script.js
+++ script.js
@@ -1,5 +1,82 @@
 const mario = document.querySelector('.mario');
 const cano = document.querySelector('.cano');
+const btn = document.getElementById('reloadButton')
+const pause = document.getElementById('pause')
+const btnResume = document.getElementById('resume')
+
+document.getElementById('reloadButton').addEventListener('click', function() {
+    location.reload();
+});
+
+document.getElementById('pause').addEventListener('click', function() {
+    const canoAnimationName = window.getComputedStyle(cano).animationName;
+
+    if (canoAnimationName === 'cano-animation') {
+        cano.classList.add('pause-animation');
+        pause.disabled = true;
+        btnResume.style.display = 'flex'
+    }
+});
+
+document.getElementById('resume').addEventListener('click', function() {
+    cano.classList.remove('pause-animation');
+    btnResume.style.display = 'none'
+    pause.disabled = false;
+});
+
+document.getElementById('level').addEventListener('change', function() {
+    var selectedValue = this.value;
+    if (selectedValue === '1') {
+        cano.style.animation = 'cano-animation 3s infinite reverse'
+        btnResume.style.display = 'none'
+        pause.style.display = 'none'
+    }
+
+    if (selectedValue === '2') {
+        cano.style.animation = 'cano-animation 3s infinite alternate-reverse'
+        btnResume.style.display = 'none'
+        pause.style.display = 'none'
+    }
+
+    if (selectedValue === '3') {
+        cano.style.animation = 'cano-animation 3s infinite normal'
+        btnResume.style.display = 'none'
+        pause.style.display = 'none'
+    }
+});
+
+document.getElementById('velocidade').addEventListener('change', function() {
+    var selectedValue = this.value;
+    if (selectedValue === '1') {
+        cano.style.animation = 'cano-animation 5s infinite reverse'
+        btnResume.style.display = 'none'
+        pause.style.display = 'none'
+    }
+
+    if (selectedValue === '2') {
+        cano.style.animation = 'cano-animation 4s infinite reverse'
+        btnResume.style.display = 'none'
+        pause.style.display = 'none'
+    }
+
+    if (selectedValue === '3') {
+        cano.style.animation = 'cano-animation 3s infinite reverse'
+        btnResume.style.display = 'none'
+        pause.style.display = 'none'
+    }
+
+    if (selectedValue === '4') {
+        cano.style.animation = 'cano-animation 2s infinite reverse'
+        btnResume.style.display = 'none'
+        pause.style.display = 'none'
+    }
+
+    if (selectedValue === '5') {
+        cano.style.animation = 'cano-animation 1s infinite reverse'
+        btnResume.style.display = 'none'
+        pause.style.display = 'none'
+    }
+});
 
 const pulo = () => {
     mario.classList.add('pulo');
@@ -22,6 +99,8 @@ const loop = setInterval(() => {
         mario.src = './imagens/game-over.png';
         mario.style.width = '75px';
         mario.style.marginLeft = '50px';
+
+        btn.style.display = 'flex'
 
         clearInterval(loop);
     }
